891d1de00a59e4582e64473eb4e36a74d0a5a4b8
Esteban <135396733+Jesteban247@users.noreply.github.com>
2024-02-13 14:01:25 +0000

    Update T_4

--- Talleres/T_4/main.cpp
+++ Talleres/T_4/main.cpp
@@ -1,75 +1,33 @@
 #include <iostream>
 using namespace std;
 
-
-int menu()
-{
-    int opcion;
-    cout << "MENU" << endl;
-    cout << "1. Piramide" << endl;
-    cout << "2. Numeros primos" << endl;
-    cout << "3. Fibonacci" << endl;
-    cout << "4. Salir" << endl;
-    cin >> opcion;
-    cout << "-----------------" << endl << endl;
-    return opcion;
-}
-
-void Pyramid() {
-    for(int i = 1; i <= 9; i++) {
-        for(int j = i; j <= 9; j++) {
-            cout << j;
-        }
-        cout << endl;
-    }
-}
-
-void Prime(int n)
-{
-    int i, m=0, flag=0;
-    m = n/2;
-    for(i = 2; i <= m; i++)
-    {
-        if(n%i == 0)
-        {
-            cout<<"Numero no primo"<<endl;
-            flag=1;
-            break;
-        }
-    }
-    if (flag==0)
-        cout << "Numero primo" << endl;
-
-}
-
-void Fibonacci(int longitud)
-{
-    //Default values: 0,1
-    //Long=4 -> 0 1 1 2 3 5
-    int a = 0, b = 1, c;
-    cout << a << " " << b << " ";
-    for (int i = 2; i < longitud+2; i++)
-    {
-        c = a + b;
-        cout << c << " ";
-        a = b;
-        b = c;
-    }
-    cout << endl;
-    
-}
-
 int main()
 {
     int opcion;
+    
+    // Fibonacci
+    int a = 0, b = 1, c;
+    
     do
     {
-        opcion = menu();
+        cout << "MENU" << endl;
+        cout << "1. Piramide" << endl;
+        cout << "2. Numeros primos" << endl;
+        cout << "3. Fibonacci" << endl;
+        cout << "4. Salir" << endl;
+        cin >> opcion;
+        cout << "-----------------" << endl << endl;
+
         switch (opcion)
         {
             case 1:
                 cout << "Piramide" << endl;
-                Pyramid();
+                for(int i = 1; i <= 9; i++) {
+                    for(int j = i; j <= 9; j++) {
+                        cout << j;
+                    }
+                    cout << endl;
+                }
                 break;
             case 2:
                 cout << "Numeros primos" << endl;
@@ -79,7 +37,19 @@ int main()
                 while (n >0)
                 {
                     cout << "Numero: " << n << endl;
-                    Prime(n);
+                    int i, m=0, flag=0;
+                    m = n/2;
+                    for(i = 2; i <= m; i++)
+                    {
+                        if(n%i == 0)
+                        {
+                            cout<<"Numero no primo"<<endl;
+                            flag=1;
+                            break;
+                        }
+                    }
+                    if (flag==0)
+                        cout << "Numero primo" << endl;
                     cout << "Ingrese otro numero: ";
                     cin >> n;
                 }
@@ -89,7 +59,18 @@ int main()
                 cout << "Ingrese la longitud de la serie: ";
                 int longitud;
                 cin >> longitud;
-                Fibonacci(longitud);
+                //Default values: 0,1
+                //Long=4 -> 0 1 1 2 3 5
+                
+                cout << a << " " << b << " ";
+                for (int i = 2; i < longitud+2; i++)
+                {
+                    c = a + b;
+                    cout << c << " ";
+                    a = b;
+                    b = c;
+                }
+                cout << endl;
                 break;
             case 4:
                 cout << "Saliendo..." << endl;
