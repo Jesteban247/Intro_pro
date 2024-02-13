a37576820c65bdc22939ee79ddddfdb5a8362e97
Esteban <135396733+Jesteban247@users.noreply.github.com>
2024-02-13 13:45:48 +0000

    Update_T3

--- Talleres/T_3/main.cpp
+++ Talleres/T_3/main.cpp
@@ -4,59 +4,45 @@ using namespace std;
 
 int main() 
 {
-    int cant,mayor=0, menor=0;
+    int cant, mayor = 0, menor = 0;
 
     // Leer el txt
     ifstream archivo;
     archivo.open("info.txt");
     archivo >> cant;
-    for (int i = 0; i < cant; i++)
+
+     for (int i = 0; i < cant; i++)
+{
+    int numero, it = 0;
+    archivo >> numero;
+
+    while (numero != 6174 && it < 8)
     {
-        int numero, it=0;
-        archivo >> numero;
-    
-        while (numero != 6174 && it < 8)
+        int a = numero / 1000;
+        int b = (numero / 100) % 10;
+        int c = (numero / 10) % 10;
+        int d = numero % 10;
+
+        // Sort the digits in ascending order
+        for (int j = 0; j < 3; j++)
         {
-            int num = numero;
-            int digitos[4];
-            for (int i = 0; i < 4; i++)
-            {
-                digitos[i] = num % 10;
-                num /= 10;
-            }
-
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    if (digitos[j] < digitos[j+1])
-                    {
-                        int temp = digitos[j];
-                        digitos[j] = digitos[j+1];
-                        digitos[j+1] = temp;
-                    }
-                }
-            }
-
-            mayor = 0;
-            for (int i = 0; i < 4; i++)
-            {
-                mayor = mayor * 10 + digitos[i];
-            }
-
-            menor = 0;
-            for (int i = 3; i >= 0; i--)
-            {
-                menor = menor * 10 + digitos[i];
-            }
-
-            numero = mayor - menor;
-            //cout << mayor << " - " << menor << " = " << numero << endl;
-            it++;
+            if (a > b) { int temp = a; a = b; b = temp; }
+            if (b > c) { int temp = b; b = c; c = temp; }
+            if (c > d) { int temp = c; c = d; d = temp; }
         }
 
-        cout << "Iteraciones: " << it << endl;
+        // Rearrange the digits
+        int num_menor = a * 1000 + b * 100 + c * 10 + d;
+        int num_mayor = d * 1000 + c * 100 + b * 10 + a;
+
+        numero = num_mayor - num_menor;
+
+
+        it++;
     }
+
+    cout << "Iteraciones: " << it << endl;
+}
 
     return 0;
 }
